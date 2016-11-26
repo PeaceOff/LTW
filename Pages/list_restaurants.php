74f6ad431e9f6813e46c0402273144b427808fe0
joaocsf <noobcsf@hotmail.com>
2016-11-26 19:32:36 +0000

    Fixed Paths

--- Pages/list_restaurants.php
+++ Pages/list_restaurants.php
@@ -1,12 +1,12 @@
 <?php
 
-include_once('templates/header.php');
+include_once('../templates/header.php');
 
 <h1>RestaurantWizz</h1>
 
 
 
-include_once('templates/footer.php');
+include_once('../templates/footer.php');
 
 
 ?>
---
+++ database/action_login.php
@@ -0,0 +1,9 @@
+<?php
+
+  $usr = $_POST['username'];
+
+  echo $usr;
+
+  header('Location: ../Pages/home.php');
+
+?>
