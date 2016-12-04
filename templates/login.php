c4da771d1984103babbae285ccb67b10237f700f
Jose Martins <lluismmartins7@gmail.com>
2016-12-04 19:41:42 +0000

    minor fix

--- templates/login.php
+++ templates/login.php
@@ -8,7 +8,7 @@
 ?>
 <a href="../pages/user_profile.php?username=<?php echo $_SESSION['username']?>"> <img src="<?php echo $path ?>"/> </a>
 <?php } ?>
-<label> Logged in as: <?php echo $_SESSION['username'] ?> </label>
+<label> Logged in as:<a href="../pages/user_profile.php?username=<?php echo $_SESSION['username']?>"><?php echo $_SESSION['username'] ?></a></label>
 <a href="../database/action_logout.php">Logout</a>
 
 <?php }else{ ?>
