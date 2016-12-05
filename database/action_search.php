78b5f905ddb24fd490f1b3c1dc170ad74e66cf2f
Jose Martins <lluismmartins7@gmail.com>
2016-12-05 19:58:25 +0000

    minor fix

--- database/action_search.php
+++ database/action_search.php
@@ -12,34 +12,34 @@ $typeId=$_POST['type'];
 
 $results = getRestaurantBySearch($search,$typeId,false);
 
+?>
+
+<div class="page">
+  <div id="restaurantList">
+    <ul id="restaurants">
+
+<?php
 foreach ($results as $result) {
   if($typeId == -1)
       $link = "../pages/show_restaurant.php?id=" . $result['id'];
   else
       $link = "../pages/show_restaurant.php?id=" . $result['restaurant_id'];
 
-?>
-
-<div class="page">
-  <div id="restaurantList">
-    <ul id="restaurants">
-      <?php
       echo '<li> Name: ' . $result['name'] . '</br>';
       echo 'Description: ' . $result['description'];
       echo 'Type Id: ' . $result['type_id'] . '</li>';
       echo '<a href=' . $link . '> View Restaurant Info </a>';
-      ?>
+  }
+?>
+
+
     </ul>
   </div>
 </div>
 
 
-<?php } ?>
 
 
-?>
-
-</br>
 <form id="searchForm" action="../pages/home.php" method="post">
     <input type="submit" value="Back">
 </form>
