10dce21ed4c334df90723c59a22a2045382da744
Jose Martins <lluismmartins7@gmail.com>
2016-12-05 20:04:08 +0000

    update on add restaurant image

--- database/action_add_image.php
+++ database/action_add_image.php
@@ -9,10 +9,10 @@
       header('Location: ' . $_SERVER['HTTP_REFERER']);
       exit();
   }
-
-  $restaurant_id = htmlentities($_POST['restaurant_id'], ENT_QUOTES, "UTF-8");
-  addImage($restaurant_id,$_FILES['image']['tmp_name']);
-
+  if($_FILES['image']['size'] != 0){
+    $restaurant_id = htmlentities($_POST['restaurant_id'], ENT_QUOTES, "UTF-8");
+    addImage($restaurant_id,$_FILES['image']['tmp_name']);
+  }
 
   header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
